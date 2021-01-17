e0ea41d1dbe73f15d20b71585a55ffbeec1bfabd
allexis <allexissoares@gmail.com>
2021-01-17 13:05:01 -0300

    redux saga config

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -18,7 +18,7 @@ function Catalog() {
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <CatalogItem key={product} product={product} />
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
