5a9dd0f138282f7ad68031807e3aa7523cf0968c
allexis <allexissoares@gmail.com>
2021-01-17 12:10:37 -0300

    modifying state

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,8 +2,28 @@ const INITAL_STATE = {
   items: []
 }
 
-function cart() {
-  return INITAL_STATE;
+function cart(state = INITAL_STATE, action) {
+  console.log(state, action)
+
+  switch(action.type) {
+    case 'ADD_PRODUCT_TO_CART': {
+      const { product } = action.payload;
+
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            product,
+            quantity: 1,
+          }
+        ]
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
 
 export default cart;
