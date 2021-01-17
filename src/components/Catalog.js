66f88883f5ebbb637e492649d303f2aa6351d772
allexis <allexissoares@gmail.com>
2021-01-17 10:56:42 -0300

    store config

---
+++ src/components/Catalog.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+
+function Catalog() {
+  const catalog = useSelector(state => state)
+
+  console.log(catalog);
+
+  return <h1>Hello World</h1>;
+};
+
+export default Catalog;
